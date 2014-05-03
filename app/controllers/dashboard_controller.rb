bb085324f742d341722f9aec35164a1b62f52893
sunila <sunilasaqib1@gmail.com>
2014-05-03 10:04:18 -0600

    paperclip successfully tested on heroku.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,8 +4,8 @@ class DashboardController < ApplicationController
 	
   def index
   # un comment this and dashboard/index code to check how the images are saved. 
-  @user = Medium.find(1)
-  @user1 = Medium.find(2)
+  #@user = Medium.find(1)
+  #@user1 = Medium.find(2)
   #@user2 = Medium.find(3)
   end
 end
