d77a345e08fafb5b86de698c8ddf99e5a38c5532
sunila <sunilasaqib1@gmail.com>
2014-05-03 10:10:51 -0600

    assets clean up

--- public/assets/application-99a18ffc17a269a5d459a7e1c094c8e4.js
+++ public/assets/application-99a18ffc17a269a5d459a7e1c094c8e4.js
@@ -11340,21 +11340,36 @@ return jQuery;
 
 
 }).call(this);
+/*
 $(function(){
+
+//<div class="file-input-wrapper">
+ // <button class="btn-file-input">Custom Upload</button>
+  //<input type="file" name="file" />
+//</div>
   var count = parseInt($('#count').val());
   var pic = document.getElementById('pictures');
   for (var i = 0; i < count; i++ ){
-    var p1 = document.createElement("p");
-    var p2 = document.createElement("p");
+    var d1 = document.createElement("div");
+    $(d1).addClass("file-input-wrapper");
+    var b1 = document.createElement("button");
+    $(b1).addClass("btn-file-input");
+    $(b1).html("Upload attachment");
+    //var p1 = document.createElement("p");
+    //var p2 = document.createElement("p");
     var pn = document.createElement("input");
     $(pn).attr({
       type:'file',
       id:'medium_o'+i+'',
       name:'medium_o'+i+''
     });
-    pic.appendChild(p1);
-    pic.appendChild(pn);
-    pic.appendChild(p2);
+    d1.appendChild(pn);
+    d1.appendChild(b1);
+    pic.appendChild(d1);
+    
+    //pic.appendChild(p1);
+    //pic.appendChild(pn);
+    //pic.appendChild(p2);
 
   }
   $(pic).show();
@@ -11363,19 +11378,25 @@ $(function(){
     var count = $('#count').val();
     $('#count').val(parseInt(count) + 1);
     var pic = document.getElementById('pictures');
-    var p1 = document.createElement("p");
-    var p2 = document.createElement("p");
+    var d1 = document.createElement("div");
+    $(d1).addClass("file-input-wrapper");
+    var b1 = document.createElement("button");
+    $(b1).addClass("btn-file-input");
+    $(b1).html("Upload attachment");
     var pn = document.createElement("input");
     $(pn).attr({
       type:'file',
-      id:'medium_o'+count+'',
-      name:'medium_o'+count+''
+      id:'medium_o'+i+'',
+      name:'medium_o'+i+''
     });
-    pic.appendChild(p1);
-    pic.appendChild(pn);
-    pic.appendChild(p2); 
+    d1.appendChild(b1);
+    d1.appendChild(pn);
+    pic.appendChild(d1);
   });
 }); 
+*/
+
+;
 (function() {
 
 
